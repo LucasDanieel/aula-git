14e97bd90443429bc512e92254e6e57348ae4c15
LucasDanieel <lucasdaniel545@gmail.com>
2021-09-02 12:23:15 -0300

    Acrescentada a pergunta Como você está hoje

--- Projeto-test/Program.cs
+++ Projeto-test/Program.cs
@@ -8,6 +8,8 @@ namespace Projeto_test
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
+            
             Console.ReadLine();
         }
     }
